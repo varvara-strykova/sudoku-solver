20ca13fea6f38cb1e7ac0d3c4fa88bd1ad1f9f39
varvara-strykova <strykova.varvara@gmail.com>
2026-02-19 22:41:25 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,22 +1,17 @@
-
-
 import java.io.File;
 import java.util.Scanner;
 
 public class Board{
 	
 	/*The Sudoku Board is made of 9x9 cells for a total of 81 cells.
-	 * In this program we will be representing the Board using a 2D Array of cells.
-	 * 
+	 * In this program I will be representing the Board using a 2D Array of cells. 
 	 */
 
 private Cell[][] board = new Cell[9][9];
-	
 	//The variable "level" records the level of the puzzle being solved.
 	private String level = "";
-	
-	///TODO: CONSTRUCTOR
-	//This must initialize every cell on the board with a generic cell.  It must also assign all of the boxIDs to the cell. 
+
+	//This initializes every cell on the board with a generic cell and assigns the boxIDs to the cell. 
 	public Board()
 	{
 		for(int x = 0; x < 9; x++)
@@ -27,16 +22,10 @@ private Cell[][] board = new Cell[9][9];
 			}
 	}
 	
-	
-	
-	///TODO: loadPuzzle
-	/*This method will take a single String as a parameter.  The String must be either "easy", "medium" or "hard"
+	/*This method takes a single String as a parameter.  The String must be either "easy", "medium" or "hard"
 	 * If it is none of these, the method will set the String to "easy".  The method will set each of the 9x9 grid
 	 * of cells by accessing either "easyPuzzle.txt", "mediumPuzzle.txt" or "hardPuzzle.txt" and setting the Cell.number to
-	 * the number given in the file. 
-	 *
-	 * This must also set the "level" variable
-	 * TIP: Remember that setting a cell's number affects the other cells on the board.
+	 * the number given in the file.
 	 */
 	public void loadPuzzle(String level) throws Exception
 	{
@@ -45,8 +34,8 @@ private Cell[][] board = new Cell[9][9];
 		if(level.contentEquals("medium"))
 			fileName = "mediumPuzzle.txt";
 		else if(level.contentEquals("hard"))
-			fileName = "/Users/varvara_strykova/Desktop/Varvara S. Java Workspace CS 30 HS/Sudoku Solver/src/hardPuzzle.txt";
-   else if(level.contentEquals("oni"))
+			fileName = "hardPuzzle.txt";
+   		else if(level.contentEquals("oni"))
 			fileName = "oni.txt";
 		
 		Scanner input = new Scanner (new File(fileName));
@@ -63,9 +52,7 @@ private Cell[][] board = new Cell[9][9];
 		   
 	}
 	
-	///TODO: isSolved
 	/*This method scans the board and returns TRUE if every cell has been solved.  Otherwise it returns FALSE
-	 *
 	 */
 	public boolean isSolved()
 	{
@@ -74,12 +61,12 @@ private Cell[][] board = new Cell[9][9];
 				if(board[x][y].getNumber()==0)
 					return false;
 			
-		}
+			}
 		}
 		return true;
 	}
-	///TODO: DISPLAY
-	/*This method displays the board neatly to the screen.  Please see the sample output from the assignment to see the format required.
+	
+	/*This method displays the board neatly to the screen.
 	 */
 	public void display()
 	{
@@ -88,21 +75,16 @@ private Cell[][] board = new Cell[9][9];
 			for(int y = 0; y<9; y++) {
 				System.out.print(board[x][y].getNumber()+ " ");
 				}
-			
 				System.out.println();
 			}
 		}
 	
-	
-	///TODO: solve
-	/*This method solves a single cell at x,y for number.  It also must adjust the potentials of the remaining cells in the same row,
+	/*This method solves a single cell at x,y for number.  It also adjusts the potentials of the remaining cells in the same row,
 	 * column, and box.
 	 */
 	public void solve(int x, int y, int number)
 	{
-		
 		board[x][y].setNumber(number);
-		
 		
 		for(int j = 0; j < 9; j++)
 			if(j != y)
@@ -214,14 +196,13 @@ private Cell[][] board = new Cell[9][9];
 		
 	}
 	
-	///TODO: logic1
 	/*This method searches each row of the puzzle and looks for cells that only have one potential.  If it finds a cell like this, it solves the cell
 	 * for that number. This also tracks the number of cells that it solved as it traversed the board and returns that number.
 	 */
 	public int logic1()
 	{
 		int changesMade = 0;
-		
+
 		for(int x =0; x < 9; x++) {
 			for(int y = 0; y<9; y++) {
 				if(board[x][y].numberOfPotentials()==1 && board[x][y].getNumber()==0) {
@@ -239,12 +220,10 @@ private Cell[][] board = new Cell[9][9];
 					
 	}
 	
-	///TODO: logic2
 	/*This method searches each row for a cell that is the only cell that has the potential to be a given number.  If it finds such a cell and it
-	 * is not already solved, it solves the cell.  It then does the same thing for the columns.This also tracks the number of cells that
+	 * is not already solved, it solves the cell.  It then does the same thing for the columns. This also tracks the number of cells that
 	 * it solved as it traversed the board and returns that number.
 	 */
-	
 	public int logic2()
 	{
 		int changesMade = 0;
@@ -301,13 +280,11 @@ private Cell[][] board = new Cell[9][9];
 		return changesMade;
 	}
 	
-	///TODO: logic3
 	/*This method searches each box for a cell that is the only cell that has the potential to be a given number.  If it finds such a cell and it
 	 * is not already solved, it solves the cell. This also tracks the number of cells that it solved as it traversed the board and returns that number.
 	 */
 	public int logic3()
 	{
-	
 		int changesMade = 0;
 		
 		for(int x =0; x < 9; x++) {
@@ -324,16 +301,9 @@ private Cell[][] board = new Cell[9][9];
 	}
 	
 	
-	///TODO: logic4
 		/*This method searches each row for the following conditions:
 		 * 1. There are two unsolved cells that only have two potential numbers that they can be
 		 * 2. These two cells have the same two potentials (They can't be anything else)
-		 *
-		 * Once this occurs, all of the other cells in the row cannot have these two potentials.  Write an algorithm to set these two potentials to be false
-		 * for all other cells in the row.
-		 *
-		 * Repeat this process for columns and rows.
-		 *
 		 * This also tracks the number of cells that it solved as it traversed the board and returns that number.
 		 */
 	
@@ -405,7 +375,6 @@ private Cell[][] board = new Cell[9][9];
 	}
 	
 	
-	///TODO: errorFound
 	/*This method scans the board to see if any logical errors have been made.  It can detect this by looking for a cell that no longer has the potential to be
 	 * any number.
 	 */
@@ -426,3 +395,4 @@ private Cell[][] board = new Cell[9][9];
 	
 
 }
+
